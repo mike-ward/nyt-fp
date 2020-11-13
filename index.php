2a93c217ae41942dcc7b14e1e303cb4e3396b010
Mike Ward <mike@mike-ward.net>
2020-11-13 14:32:19 -0600

    remove imageick

--- index.php
+++ index.php
@@ -21,25 +21,8 @@ if(!$exists) {
 	$pathToPdf="https://static01.nyt.com/images/".$yesterday."/nytfrontpage/scan.pdf";
 }
 
-// you needed to have Imagick compiled in PHP otherwise this won't work
-$im = new Imagick();
-
-// So this panel is 1440x2550, 600dpi, it doesn't really like PNG so we are converting a PDF to JPG (sue me)
-// get it here, compression to nothing, output it
-$im->setResolution(600,600);
-$im->readimage($pathToPdf);
-$im->setImageCompressionQuality(100);
-$im->setImageFormat('jpg');
-$im->stripImage();
-$im->scaleImage(1440,2550);
-$im->writeImage($outputfile);
-$im->clear();
-$im->destroy();
-
-// and display it with nice margins
-
 ?>
 <meta http-equiv="refresh" content="21600">
 <body style="margin-top:-100px;margin-left:-27px">
-<img width=102% height=108% src='<?=$outputfile?>?v=<?=date('Ymdhmi')?>' />
+<p><?=$pathToPdf?><p>
 </body>
